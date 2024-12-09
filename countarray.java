a9e56d1cb825fe01eee866c031a1fc45421cbc58
likkib2005 <likkilikitha214@gmail.com>
2024-12-09 12:27:18 +0530

    Add files via upload

---
+++ countarray.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class countarray
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        arr[i]=sc.nextInt();
+        //finding maximumnumber//
+        Arrays.sort(arr);
+        int max=arr[n-1];
+        //find count array//
+        int c[]=new int[max+1];
+        for(int i=0;i<n;i++)
+        c[arr[i]]++;
+        System.out.println(Arrays.toString(c));
+    }
+}
